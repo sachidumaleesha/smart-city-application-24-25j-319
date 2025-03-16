16e294362ce1299e71a7c390a5c7ae092a876fec
Nipuna Nadeeshan <IT21166556@my.sliit.lk>
2025-03-17 02:28:06 +0530

    initchange

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,13 +6,14 @@ def create_app():
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # Register accident detection blueprint
-    from app.routes.accidentDetection import accident_bp
-    from app.routes.youtubeDetection import youtube_bp
 
+    from app.routes.accidentDetection.youtubeDetection import youtube_bp
+    from app.routes.accidentDetection.accident import accident_bp
     from app.routes.wasteManagement.wasteTypeDetection import wasteManagement_bp
 
-    app.register_blueprint(accident_bp, url_prefix="/api/accidentDetection")
+    
     app.register_blueprint(wasteManagement_bp, url_prefix="/api/wasteManagement")
+    app.register_blueprint(accident_bp, url_prefix="/api/accidentDetection")
     app.register_blueprint(youtube_bp, url_prefix="/api/youtubeDetection")
 
     return app
