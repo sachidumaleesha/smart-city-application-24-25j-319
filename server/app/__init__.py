b05ec44bd4663ac1d6f792e61827f59d4262d67b
NipunPalliyaguru <it21187278@my.sliit.lk>
2025-03-17 12:21:09 +0530

    diwans files added

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 
 def create_app():
-    app = Flask(_name_)
+    app = Flask(__name__)
     # CORS(app, resources={r"/api/": {"origins": ""}})
     CORS(app)  # Enable CORS for Next.js requests
 
@@ -11,12 +11,12 @@ def create_app():
 
     from app.routes.accidentDetection.youtubeDetection import youtube_bp
     from app.routes.accidentDetection.accidentDetection import accident_bp
-    # from app.routes.wasteManagement.wasteTypeDetection import wasteManagement_bp
     from .routes.surveillanceEnhancementStream import cctv_bp
     from .routes.surveillance_enhancement import cctv_control_bp
 
-    
+    # from app.routes.wasteManagement.wasteTypeDetection import wasteManagement_bp
     # app.register_blueprint(wasteManagement_bp, url_prefix="/api/wasteManagement")
+    
     app.register_blueprint(accident_bp, url_prefix="/api/accidentDetection")
     app.register_blueprint(youtube_bp, url_prefix="/api/youtubeDetection")
 
@@ -27,3 +27,4 @@ def create_app():
     app.register_blueprint(cctv_control_bp, url_prefix="/cctv")
 
     return app
+
