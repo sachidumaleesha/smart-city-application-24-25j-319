c2194ddb73d44c1a6623c11444aec9a2916bb159
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2025-03-17 12:08:23 +0530

    deploy waste-classification model on railway set URL path

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,9 +7,9 @@ def create_app():
 
     # Import and register blueprints (routes)
     from app.routes.accidentDetection import accident_bp
-    from app.routes.wasteManagement.wasteTypeDetection import wasteManagement_bp
+    # from app.routes.wasteManagement.wasteTypeDetection import wasteManagement_bp
 
     app.register_blueprint(accident_bp, url_prefix="/api/accidentDetection")
-    app.register_blueprint(wasteManagement_bp, url_prefix="/api/wasteManagement")
+    # app.register_blueprint(wasteManagement_bp, url_prefix="/api/wasteManagement")
 
     return app
